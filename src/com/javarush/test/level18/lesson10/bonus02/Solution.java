473c7564f35ddb94ef50287f6d9302602cdfd485
Евгений <shipov.foto@mail.ru>
2016-08-31 16:03:44 +0300

    add level18, lesson10, bonus02

--- src/com/javarush/test/level18/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus02/Solution.java
@@ -22,21 +22,57 @@ id productName price quantity
 19847983Куртка для сноубордистов, разм10173.991234
 */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class Solution {
+    private static Formatter formatter = new Formatter();
+
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = reader.readLine();
-        String data = "";
+        Scanner scanner1 = new Scanner(System.in);
+        String fileName = scanner1.nextLine();
+        scanner1.close();
 
-        for (int i = 0; i < args.length; i++)
-        {
-            data +=args[i];
+        Scanner scanner2 = new Scanner(new FileInputStream(fileName));
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+
+        String operation = args[0];
+        String productName = getProductName(args);
+        String price = args[args.length - 2];
+        String quantity = args[args.length - 1];
+
+        if (operation.equals("-c")) {
+            int max = findMaximumId(scanner2);
+            max++;
+            String line = String.format("%-8s%-30s%-8s%-4s", max, productName, price, quantity);
+            addInFile(writer, line);
+        }
+    }
+
+    private static int findMaximumId(Scanner scanner) throws IOException {
+        int max = Integer.MIN_VALUE;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().substring(0, 8).trim();
+            int id = Integer.parseInt(line);
+            if (id > max) max = id;
+        }
+        scanner.close();
+        return max;
+    }
+
+    private static void addInFile(PrintWriter writer, String line) throws IOException {
+        writer.println(line);
+        writer.close();
+    }
+
+    private static String getProductName(String[] args) {
+        String productName = args[1];
+        for (int i = 2; i < args.length - 2; i++) {
+            productName = productName + " " + args[i];
         }
 
-
+        return productName;
     }
 }
