56b23b21bc002d36fac1ba15e8afadf67c5d3238
Евгений <shipov.foto@mail.ru>
2016-09-19 16:04:45 +0300

    add level19, lesson10, home 9-10

---
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.test.level19.lesson10.home09;
+
+/* Контекстная реклама
+В методе main подмените объект System.out написанной вами реадер-оберткой
+Ваша реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stream);
+        System.setOut(printStream);
+
+        testString.printSomething();
+        String[] result = stream.toString().split("\n");
+        System.setOut(out);
+
+        int x = 0;
+        for (String s : result) {
+            System.out.println(s);
+            x++;
+            if (x % 2 == 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
