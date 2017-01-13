4813bae5232136f9fb42129609f9250ff3a0268e
Евгений <shipov.foto@mail.ru>
2017-01-13 18:55:46 +0300

    add level22, lesson18  not finish

--- src/com/javarush/test/level22/lesson18/big01/FigureFactory.java
+++ src/com/javarush/test/level22/lesson18/big01/FigureFactory.java
@@ -3,8 +3,7 @@ package com.javarush.test.level22.lesson18.big01;
 /**
  * Клсс FigureFactory отвечает за создание объектов-фигурок.
  */
-public class FigureFactory
-{
+public class FigureFactory {
     /**
      * Набор из шести шаблонов для фигурок
      */
@@ -37,8 +36,7 @@ public class FigureFactory
     /**
      * Метод выбирает случайный шаблон и создает с ним новую фигурку.
      */
-    public static Figure createRandomFigure(int x,int y)
-    {
+    public static Figure createRandomFigure(int x, int y) {
         int index = (int) (Math.random() * 6);
         return new Figure(x, y, BRICKS[index]);
     }
