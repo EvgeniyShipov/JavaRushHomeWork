6f9cdd0a2c74fae30afc625c2b759872719e30b7
Евгений <shipov.foto@mail.ru>
2017-02-07 17:29:24 +0300

    add level25, lesson16, big01 not finished

--- src/com/javarush/test/level25/lesson16/big01/Ufo.java
+++ src/com/javarush/test/level25/lesson16/big01/Ufo.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level25.lesson16.big01;
 
 public class Ufo extends BaseObject {
+    private static int step = 0;
 
     private static int[][] matrix = {
             {1, 1, 1, 1, 1},
@@ -19,14 +20,18 @@ public class Ufo extends BaseObject {
     }
 
     public void move() {
+        step++;
         double dx = Math.random() * 2 - 1;
         double dy = Math.random() * 2 - 1;
         x = x + dx;
         y = y + dy;
-        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+        checkBorders(radius, Space.game.getWidth() - radius + 1, Space.game.getHeight() / 2, Space.game.getHeight() + 1);
+        if (step % 10 == 0) {
+            fire();
+        }
     }
 
     public void fire() {
-
+        Space.game.getBombs().add(new Bomb(x, y));
     }
 }
